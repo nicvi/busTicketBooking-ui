2e4d5ab8a76a45e273ee464b3951e393915b0e4f
Herman <hermanx97d@gmail.com>
2024-10-01 16:55:34 -0500

    deleting comments

--- src/components/pages/TimeSelection/TimeSelection.tsx
+++ src/components/pages/TimeSelection/TimeSelection.tsx
@@ -2,53 +2,6 @@ import {useEffect, useState} from "react";
 import {useLocation} from "react-router-dom";
 import {BusBooking} from "../../templates/BusBooking";
 
-const busData = [
-    {
-        departureTime: '10:00 AM',
-        travelTime: '2h 30m',
-        arrivalTime: '12:30 PM',
-        origin: 'New York',
-        destination: 'Philadelphia',
-        isDirect: true,
-        amenities: ['wifi', 'movie', 'usb'],
-        busStatus: 'Almost Full',
-        price: '$25',
-    },
-    {
-        departureTime: '10:00 AM',
-        travelTime: '2h 30m',
-        arrivalTime: '12:30 PM',
-        origin: 'New York',
-        destination: 'Philadelphia',
-        isDirect: true,
-        amenities: ['wifi', 'movie', 'usb'],
-        busStatus: 'Almost Full',
-        price: '$25',
-    },
-    {
-        departureTime: '10:00 AM',
-        travelTime: '2h 30m',
-        arrivalTime: '12:30 PM',
-        origin: 'New York',
-        destination: 'Philadelphia',
-        isDirect: true,
-        amenities: ['wifi', 'movie', 'usb'],
-        busStatus: 'Almost Full',
-        price: '$25',
-    },
-    {
-        departureTime: '10:00 AM',
-        travelTime: '2h 30m',
-        arrivalTime: '12:30 PM',
-        origin: 'New York',
-        destination: 'Philadelphia',
-        isDirect: true,
-        amenities: ['wifi', 'movie', 'usb'],
-        busStatus: 'Almost Full',
-        price: '$25',
-    },
-];
-
 export const TimeSelection = () => {
     const location = useLocation();
     const [ticketBooking, setTicketBooking] = useState(() => {
@@ -67,7 +20,7 @@ export const TimeSelection = () => {
     return (
         <div style={{margin: '50px'}}>
             <h1>Time selection</h1>
-            <BusBooking busData={busData}/>
+            <BusBooking busData={[]}/>
         </div>
     );
 };
